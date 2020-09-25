a6bf91ebd41cad665e21763b9acd77a25b9c0bbe
Paul Milla <pamilla@microsoft.com>
2020-09-25 15:53:40 -0500

    Add helpful reminder to Connect-TDS

--- home/libs/profile-work.ps1
+++ home/libs/profile-work.ps1
@@ -10,7 +10,38 @@ function powerline() {
     Set-Theme Paradox
 }
 
-function DeployDll-CalendarLocations($tdsIp) {
+function Connect-TDS([Parameter(Mandatory=$true)]$ipAddress) {
+    if (!(Test-Path variable:adminCred) -and !(Test-Path variable:global:adminCred)) {
+        $adminCred = Get-Credential -Credential Administrator
+        $global:adminCred = $adminCred
+    }
+    Write-Host "Reminder: To use Exchange Commands load them in via: Add-PSSnapin *2010"
+    Enter-PSSession -Credential $adminCred -Authentication CredSSP -ComputerName $ipAddress 
+}
+
+function Get-SubstrateAppToken([Parameter(Mandatory=$true)]$ipAddress) {
+    if (!(Test-Path variable:adminCred) -and !(Test-Path variable:global:adminCred)) {
+        $adminCred = Get-Credential -Credential Administrator
+        $global:adminCred = $adminCred
+    }
+    Invoke-Command -ComputerName $ipAddress -Credential $adminCred -Authentication CredSSP -ScriptBlock { & "C:\Program Files\Microsoft\Exchange Test\Security\SubstrateTestTokenTool\New-SubstrateTestToken.ps1" -AzureAD AppToken -AppId "9bdb0045-3587-47f9-863a-2ca58d11e2e8" -Grants "Locations-Internal.ReadWrite","Place.ReadWrite.All" -TokenState PreTransform }
+}
+
+function Get-SubstrateUserToken([Parameter(Mandatory=$true)]$ipAddress) {
+    if (!(Test-Path variable:adminCred) -and !(Test-Path variable:global:adminCred)) {
+        $adminCred = Get-Credential -Credential Administrator
+        $global:adminCred = $adminCred
+    }
+
+    Invoke-Command -ComputerName $ipAddress -Credential $adminCred -Authentication CredSSP -ScriptBlock {
+        Add-PSSnapin *2010;
+        $org = Get-Organization |  Where {$_.Name -like "griffin*"} | Select -First 1;
+        $smtp = Get-Mailbox -Organization $org | Where { $_.Name -like "Admin*"} | Select -First 1 -ExpandProperty PrimarySmtpAddress;
+        & "C:\Program Files\Microsoft\Exchange Test\Security\SubstrateTestTokenTool\New-SubstrateTestToken.ps1" -AppId "9bdb0045-3587-47f9-863a-2ca58d11e2e8" -AzureAD UserToken -TokenState PreTransform -Grants "Locations-Internal.ReadWrite","Place.ReadWrite.All" -SmtpAddress $smtp
+    }
+}
+
+function DeployDll-CalendarLocations([Parameter(Mandatory=$true)]$tdsIp) {
     Copy-Item "${env:INETROOT}\target\dev\calendar\Microsoft.O365.Calendar.Locations\debug\amd64\Microsoft.O365.Calendar.Locations.*" `
               "\\$tdsIp\D$\MicroService\Locations\bin" -Exclude *.config
 }
