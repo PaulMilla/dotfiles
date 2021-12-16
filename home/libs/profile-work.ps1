1cbeb1ff50d2b4ac2d1aa682814dfdb793a53ea7
Paul Milla <pamilla@microsoft.com>
2021-12-16 17:05:47 -0600

    Update setings

--- home/libs/profile-work.ps1
+++ home/libs/profile-work.ps1
@@ -1,13 +1,14 @@
 # Work-Specific profile (currently @ Microsoft)
 
-Import-Module -DisableNameChecking -Name $PSScriptRoot\scripts\lib-TDS.psm1
-Import-Module -DisableNameChecking -Name $PSScriptRoot\scripts\lib-CMU.psm1
+Import-Module -Force -DisableNameChecking -Name G:\PowerShell-Libs\src\lib-TDS.psm1
+Import-Module -Force -DisableNameChecking -Name G:\PowerShell-Libs\src\lib-CMU.psm1 -ArgumentList @("G:\OlkDataApps")
+Import-Module -Force -DisableNameChecking -Name G:\PowerShell-Libs\src\lib-Cosmos.psm1
 
 ${function:cd-Locations} = { cd "C:\git\griffin\sources\dev\Calendar\src\Locations" }
 ${function:cd-ADF} = { cd "C:\git\office.outlook.owa\Cosmos\ADF" }
 ${function:cd-Git} = { cd "C:\git" }
 ${function:cd-Experimentation} = { cd "C:\git\CTExperimentation" }
-${function:cd-CMU} = { cd "C:\git\O365Core\OlkDataApps\sources\dev\CalendarMetadataUploaderV2" }
+${function:cd-OutlookML} = { cd "C:\git\TEE\TEEGit\Offline\OutlookML\Onboarding\python"; $user = "pamilla";  Write-Host "To activate environment: conda activate outlookml" }
 
 function powerline() {
     # For some reason 'opening a cmd shell > initGriffin > powershell' causes posh-git to load REALLY slow
@@ -15,38 +16,4 @@ function powerline() {
     Import-Module Posh-Git
     Import-Module Oh-My-Posh
     Set-PoshPrompt -Theme Paradox
-}
-
-function DeployDll-CalendarLocations([Parameter(Mandatory=$true)]$tdsIp) {
-    Copy-Item "${env:INETROOT}\target\dev\calendar\Microsoft.O365.Calendar.Locations\debug\amd64\Microsoft.O365.Calendar.Locations.*" `
-              "\\$tdsIp\D$\MicroService\Locations\bin" -Exclude *.config
-}
-
-function DeployAutoPilot-CalendarMetadataUploader([Parameter(Mandatory=$true)]$tdsIp) {
-    $session = TDS-GetSession $tdsIp
-
-    ## Zip CMU Autopilot, copy to TDS, and unzip
-    Write-Host -Foreground Green "Prepping autopilot files for transfer..."
-    Compress-Archive -Path "${env:INETROOT}\target\distrib\product\all\debug\amd64\CalendarMetadataUploaderAutoPilot\CalendarMetadataUploader\*" `
-                     -DestinationPath "${env:TEMP}\CalendarMetadataUploaderAutoPilot.zip" -Force
-    Copy-Item "${env:TEMP}\CalendarMetadataUploaderAutoPilot.zip" `
-              "D:\AutoPilots\CalendarMetadataUploaderAutoPilot.zip" -Force -ToSession $session
-    Invoke-Command -Session $session -ScriptBlock {
-        Expand-Archive -Path "D:\AutoPilots\CalendarMetadataUploaderAutoPilot.zip" `
-                       -DestinationPath "D:\AutoPilots\CalendarMetadataUploader" -Force
-    } | Write-Host
-
-    # Run start.bat on TDS
-    Write-Host -Foreground Green "Running start.bat..."
-    Invoke-Command -Session $session -ScriptBlock {
-        Push-Location -Path "D:\AutoPilots\CalendarMetadataUploader"
-        & cmd /c start.bat
-    } | Write-Host
-
-    # Restart IIS & Services
-    Write-Host -Foreground Green "Restarting IIS and Services..."
-    Invoke-Command -Session $session -ScriptBlock {
-        iisreset
-        Restart-Service MsExchangeMailboxAssistants
-    } | Write-Host
 }
--- home/libs/scripts/lib-CMU.psm1
+++ home/libs/scripts/lib-CMU.psm1
@@ -1,11 +1,15 @@
 [CmdletBinding()]
 param (
-    [Parameter()]
-    [string]$cmuPath = "C:\git\O365Core\OlkDataApps"
+    [Parameter(Position=0)]
+    [string]$olkDataAppsPath = "G:\OlkDataApps"
 )
 
-Import-Module -DisableNameChecking -Name $PSScriptRoot\lib-TDS.psm1
+Import-Module -Force -DisableNameChecking -Name $PSScriptRoot\lib-TDS.psm1
 
+# Aliases
+${function:cd-CMU} = { Push-Location "$olkDataAppsPath\sources\dev\CalendarMetadataUploaderV2" }
+
+# Functions
 function CMU-InitDeploy {
     param (
         [Parameter(Mandatory=$true)]
@@ -18,25 +22,17 @@ function CMU-InitDeploy {
         [string]$cmuGriffinIniPath = "$env:HOMEPATH\Downloads\CalendarMetadataUploaderV2TimeBasedProcessor.settings.ini"
     )
 
-    if (!(Test-Path "\\${tdsIp}\D$"))
-    {
-        Write-Host -ForegroundColor Yellow "No connection established to $tdsIp. Attempting 'net use' command..."
-        net use "\\${tdsIp}\D$" /u:Administrator
-        if (!(Test-Path "\\${tdsIp}\D$"))
-        {
-            throw "Could not connect to TDS machine: $tdsIp"
-        }
-    }
+    TDS-TestDirectoryConnection $tdsIp
 
     # Copy over DLLs
-    robocopy "$cmuPath\target\sources\dev\CalendarMetadataUploaderV2\Debug\netcoreapp3.1\win10-x64\" `
+    robocopy "$olkDataAppsPath\target\sources\dev\CalendarMetadataUploaderV2\Debug\netcoreapp3.1\win10-x64\" `
              "\\${tdsIp}\D$\App\CalendarMetadataUploaderV2\" `
-             /z /MIR
+             /z /MIR /MT:32
 
     Write-Host "Copying over CMI ini file for Griffin"
     Copy-Item "$cmuGriffinIniPath" "\\${tdsIp}\D$\Microservice\Griffin\IniFiles\"
 
-    Write-Host -ForegroundColor Yellow "Please edit MasterProcessorConfig.settings.ini manually. Copy over the CMU settings from the git repo here:
+    Write-Host -ForegroundColor Yellow "Please edit MasterProcessorConfig.settings.ini manually. Copy over the CMU settings from the git repo here (CalendarMetadataUploaderV2TimeBasedProcessor):
     https://o365exchange.visualstudio.com/O365%20Core/_git/Griffin?path=%2Fsources%2Fdev%2FGriffin%2Fsrc%2FControllerService%2FIniFiles%2FMasterProcessorConfig.settings.ini&version=GBusers%2Fanclawso%2FNetCoreTBA&_a=contents"
     Write-Host "Press any key to edit..."
     $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
@@ -48,14 +44,17 @@ function CMU-InitDeploy {
         Restart-Service "Substrate Auth Service 2",MSExchangeMailboxAssistants,MSExchangeStiSvc
     } | Write-Host
 
-    Write-Host -ForegroundColor Green "Done! To start CMU remote in and start the exe via:"
+    Write-Host -ForegroundColor Green "Done! To start CMU begin a remote PSSession and start the exe via:"
     Write-Host "
     TDS-Connect ${tdsIp}
     Push-Location D:\App\CalendarMetadataUploaderV2
-    ./CalendarMetadataUploaderV2.exe
+    ./start.bat
     "
-    Write-Host -ForegroundColor Green "To make griffin send a notification:
-    Start-MailboxAssistant -AssistantName CalendarMetadataUploaderV2TimeBasedProcessor -Identity <userMailbox>
+    Write-Host -ForegroundColor Green "To make griffin send a notification (from a remote PSSession):
+    Add-PSSnapin *2010
+    `$org = Get-Organization |? {`$_.Name -like `"griffin*`"} | Select -First 1;
+    `$adminMailbox = `"admin@`$org`"
+    Start-MailboxAssistant -AssistantName CalendarMetadataUploaderTimeBasedProcessor -Identity <userMailbox>
     "
     Write-Host -ForegroundColor Green "To check enabled MailboxProcessors(type2):
     https://${tdsIp}:444/griffin/registration/GetEnabledProcessors?version=2&type=2"
@@ -67,24 +66,76 @@ function CMU-Redeploy {
         [string]$tdsIp
     )
 
-    if (!(Test-Path "\\${tdsIp}\D$"))
-    {
-        Write-Host -ForegroundColor Yellow "No connection established to $tdsIp. Attempting 'net use' command..."
-        net use "\\${tdsIp}\D$" /u:Administrator
-        if (!(Test-Path "\\${tdsIp}\D$"))
-        {
-            throw "Could not connect to TDS machine: $tdsIp"
-        }
-    }
+    TDS-TestDirectoryConnection $tdsIp
 
     # Copy over DLLs
-    robocopy "$cmuPath\target\sources\dev\CalendarMetadataUploaderV2\Debug\netcoreapp3.1\win10-x64\" `
+    cd-CMU
+    & msbuild
+    robocopy "$olkDataAppsPath\target\distrib\CalendarMetadataUploaderV2\netcoreapp3.1\win10-x64\autopilot\CalendarMetadataUploaderV2" `
              "\\${tdsIp}\D$\App\CalendarMetadataUploaderV2\" `
-             /z /MIR
+             /z /MIR /MT:32 
+    Pop-Location
 }
 
-function CMU-ProcessMailbox
-{
+function CMU-Run {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$tdsIp
+    )
+    $session = TDS-GetSession $tdsIp
+
+    Invoke-Command -Session $session -ScriptBlock {
+        Push-Location "D:\App\CalendarMetadataUploaderV2"
+
+        if (!(Test-Path "start.bat")) {
+            Write-Warning "start.bat not found! Running CalendarMetadataUploaderV2.exe directly..."
+            & "./CalendarMetadataUploaderV2.exe"
+        }
+
+        Write-Host "Running CMU via start.bat..."
+        & ./start.bat
+    } | Write-Host
+}
+
+function CMU-OpenGriffinSettings {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$tdsIp
+    )
+
+    TDS-TestDirectoryConnection $tdsIp
+
+    $mailboxProcessorSettings = "\\$tdsIp\\D$\MicroService\Griffin\IniFiles\CalendarMetadataUploaderMailboxProcessor.settings.ini"
+    if (Test-Path $mailboxProcessorSettings) {
+        Write-Host -ForegroundColor Green "Opening $mailboxProcessorSettings.."
+        Start-Process $mailboxProcessorSettings
+    }
+    else {
+        Write-Error "Could not find file at $mailboxProcessorSettings"
+    }
+}
+
+function CMU-OpenSnapshotLog {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$tdsIp
+    )
+
+    TDS-TestDirectoryConnection $tdsIp
+
+    $snapshotLog = "\\$tdsIp\D$\OfficeDataLoader\Dump\NRTLoggingSdk\CMUSnapshotLog"
+    if (Test-Path $snapshotLog) {
+        Write-Host -ForegroundColor Green "Opening $snapshotLog.."
+        Start-Process $snapshotLog
+    }
+    else {
+        Write-Error "Could not find file at $snapshotLog"
+    }
+}
+
+function CMU-ProcessMailbox {
     param (
         [Parameter(Mandatory=$true)]
         [string]$tdsIp,
@@ -108,8 +159,7 @@ function CMU-ProcessMailbox
     } | Write-Host
 }
 
-function CMU-GenerateUserToken
-{
+function CMU-GenerateUserToken {
     param (
         [Parameter(Mandatory=$true)]
         [string]$tdsIp,
@@ -148,4 +198,4 @@ function CMU-GenerateUserToken
     } | Write-Host
 }
 
-Export-ModuleMember -Function *
+Export-ModuleMember -Function cd-CMU,CMU-*
