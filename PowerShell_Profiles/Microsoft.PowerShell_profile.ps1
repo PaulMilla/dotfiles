ea38f9bb64f48cdd60818aefafb0b82160012552
Paul Milla <pamilla@microsoft.com>
2022-01-07 17:11:45 -0600

    Fixed problem by granting account write permissions to TEMP dir

--- PowerShell_Profiles/Microsoft.PowerShell_profile.ps1
+++ PowerShell_Profiles/Microsoft.PowerShell_profile.ps1
@@ -3,10 +3,7 @@
 # ===========
 
 if (Test-Path "$home\libs\profile-console.ps1") {
-  # For some reason this is only working on Core now?
-  if ($PSVersionTable.PSEdition -eq "Core") {
-    . $home\libs\profile-console.ps1
-  }
+  . $home\libs\profile-console.ps1
 }
 
 ## TODO: Create profile for Visual Studio
