b7ac052db5912e791f4bdf77d472316d941c3d39
Paul Milla <pamilla@microsoft.com>
2023-04-03 18:47:13 -0500

    Update mac changes

--- bootstrap-apps.sh
+++ bootstrap-apps.sh
@@ -4,7 +4,7 @@ if test ! $(which brew); then
   echo "Installing homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   if test $? -ne 0; then
-    echo "Homebrew installation failed. Try installing xcode command line tools and running the script again."
+    echo "Homebrew installation failed. Try installing xcode command line tools and running the script again:"
     echo "  xcode-select --install"
     exit 1
   fi
@@ -18,7 +18,11 @@ brew install zsh
 brew install maccy
 brew install --cask raycast
 brew install --cask rectangle
+brew install exa
 
 # Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+brew tap homebrew/cask-fonts
+brew update
+brew install --cask font-hack-nerd-font
