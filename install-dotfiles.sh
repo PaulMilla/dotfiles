4d651215a5e54404abe666bc4a07c3302b7357ba
Paul Milla <pamilla@microsoft.com>
2023-12-31 14:54:16 -0600

    Add settings for Karabiner Elements

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -2,7 +2,8 @@
 # Using expansion modifiers from zsh to get the path of the script
 # For more info see http://zsh.sourceforge.net/Doc/Release/Expansion.html#Modifiers
 
-home_dir="${0:a:h}/home"
+git_root="${0:a:h}"
+home_dir="$git_root/home"
 echo "Mounting $home_dir to \$HOME directory: $HOME"
 
 for filename in $home_dir/.*; do
@@ -11,3 +12,12 @@ for filename in $home_dir/.*; do
         ln -s -f $filename ~
     fi
 done
+
+echo ""
+echo "Mounting custom settings..."
+
+# Mount custom/karabiner-elements/paul.json to the karabiner complex_modifications folder
+filename="$git_root/custom/karabiner-elements/paul.json"
+target="$HOME/.config/karabiner/assets/complex_modifications/"
+echo "Creating link for $filename to $target"
+ln -s -f $filename $target
